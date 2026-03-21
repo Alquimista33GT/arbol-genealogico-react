48bf9f015686f9534709054e775f5cbad26f0214
Nicolas <crazymotorsgt@gmail.com>
2026-03-21 11:41:34 -0600

    login firebase funcionando

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBbjGT0Id6IEP67hs090FKp8Yh5DAgzmQg",
+  authDomain: "arbol-genalogico.firebaseapp.com",
+  projectId: "arbol-genalogico",
+  storageBucket: "arbol-genalogico.firebasestorage.app",
+  messagingSenderId: "825872162433",
+  appId: "1:825872162433:web:dc6263f62a0bbaa773b780",
 };
 
 const app = initializeApp(firebaseConfig);
